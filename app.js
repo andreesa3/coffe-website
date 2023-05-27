894736f761417c790dc4476e6237c9e5da6d2679
andreesa3 <90782556+andreesa3@users.noreply.github.com>
2023-05-27 14:06:45 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 const hamburger = document.querySelector(".hamburger");
 const closeIcon = document.querySelector(".close-icon");
 const links = document.querySelector(".links");
+const body = document.querySelector("body");
 
 hamburger.addEventListener("click", openSidebar);
 closeIcon.addEventListener("click", closeSidebar);
 
 function openSidebar() {
-  links.classList.add('active');
-  closeIcon.style.display = 'block';
+  links.classList.add("active");
+  closeIcon.style.display = "block";
 }
 function closeSidebar() {
-  links.classList.remove('active');
-  closeIcon.style.display = 'none';
+  links.classList.remove("active");
+  closeIcon.style.display = "none";
 }
+
+const scrollbarContainer = document.querySelector(".scrollbar-container");
+const content = document.querySelector(".content");
+new SimpleBar(content, { autoHide: true });
+
+function checkScreenSize() {
+  let mediaQuery = window.matchMedia("(min-width: 768px)");
+
+  if (mediaQuery.matches) {
+    links.classList.remove("active");
+    body.classList.remove("no-scroll");
+    closeIcon.style.display = "none";
+  }
+}
+
+window.addEventListener("resize", checkScreenSize);
+
+checkScreenSize();
